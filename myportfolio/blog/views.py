27c74b1c3b80681af74a0fbd5a85feecae0fb8f0
Hossam <hossamhsn74@gmail.com>
2020-04-09 10:23:54 +0200

    done

--- myportfolio/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Blog
-# Create your views here.
-
-
-def index(request):
-    blogs = Blog.objects.all
-    return render(request, 'blog/base.html', {'blogs': blogs})
